693404ce2edeb3706c4f52330716ad63d2867800
김민주 <891km@ewhain.net>
2026-02-15 18:47:06 +0900

    style: 그리드 모드 랜덤 비율 레이아웃 적용

--- nextjs-helloep-task/src/components/ResultGrid.jsx
+++ nextjs-helloep-task/src/components/ResultGrid.jsx
@@ -3,17 +3,32 @@ import { useLanguage } from "@/provider/LanguageProvider";
 import { urlFor } from "@/utils/urlFor";
 import Link from "next/link";
 
+const gridItemRatio = [1.4, 1.2, 1, 1, 1, 0.8, 0.8];
+
+function getRandomIndex(seed) {
+  const random = Math.sin(seed * 9999) * 10000;
+  const normalized = random - Math.floor(random);
+  return Math.floor(normalized * gridItemRatio.length);
+}
+
 export default function ResultGrid({ posts }) {
   const { isKor } = useLanguage();
 
   return (
-    <ul className="flex flex-wrap items-baseline gap-2.5">
-      {posts.map((post) => {
+    <ul className="flex flex-wrap items-baseline gap-x-2.5 gap-y-11 after:content-[''] after:flex-1000">
+      {posts.map((post, i) => {
+        const ratio = gridItemRatio[getRandomIndex(i)];
         return (
-          <li key={post._id}>
+          <li
+            key={post._id}
+            style={{
+              flexBasis: `${240 * ratio}px`,
+            }}
+            className="grow"
+          >
             <Link
               href={`${post.slug.current}`}
-              className="flex flex-col gap-2 hover:opacity-50"
+              className="flex flex-col gap-2 hover:opacity-50 transition-opacity"
             >
               <img
                 src={urlFor(post.thumbnail.asset._ref)}
